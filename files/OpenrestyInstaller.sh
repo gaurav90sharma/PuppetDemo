e7cef1e5909a631fb3758023b3884f3798d75492
gaurav90sharma <gaurav90sharma@gmail.com>
2016-05-17 22:04:50 +0530

    Update OpenrestyInstaller.sh

--- files/OpenrestyInstaller.sh
+++ files/OpenrestyInstaller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tar zxf /tmp/openresty-1.9.7.4.tar.gz -C /tmp
-if [[ $? == 0  ]]; then
+if [[ $? == 0 ]]; then
 cd /tmp/openresty-1.9.7.4
 ./configure --with-http_ssl_module --with-luajit --with-pcre --with-pcre-jit && make && make install
 else
